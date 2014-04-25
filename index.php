e88ceda4b96591cf9b86ac03ab61975088490b29
caiackandrade <caila@ig.com.br>
2014-04-25 04:25:07 -0300

    teste pullando do pc

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+
+	<head>
+		<meta charset='utf-8'>
+		<link rel="stylesheet" href='css.css' />
+		<link rel="shortcut icon" href="img/title.png" /> 
+		<title>Dota MOC</title>
+	</head>
+	
+	<body>
+			
+		<div id="menuPrincipal">
+			
+			<div id="menu">
+		
+					<ul>
+						<li id="primeiro"><a href="index.php"> Inicio </a></li>
+						<li><a href="#"> Campeonatos </a></li>
+						<li><a href="#"> Equipes MOC </a></li>
+						<li><a href="#"> Vencedores </a></li>
+						<li><a href="#"> Sugestões </a></li>
+						<li><a href="#"> Regras </a></li>
+					</ul>
+			</div>
+				
+		</div>
+		
+		<div id="corpo">
+				
+			
+				
+				<div id="conteudo">
+						
+										
+					
+				</div>
+				
+				<div id="lateral">
+					
+				</div>
+				
+		</div>
+	
+	</body>
+
+</html>
