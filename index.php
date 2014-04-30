e693ba1f18c3e77ddd6ed431874f12be6839b2f0
Caick <caila@ig.com.br>
2014-04-29 21:56:11 -0300

    modificacoes css e adicao de pagina

--- index.php
+++ index.php
@@ -2,28 +2,25 @@
 
 	<head>
 		<meta charset='utf-8'>
-		<link rel="stylesheet" href='css.css' />
+		<link rel="stylesheet" href='main.css' />
 		<link rel="shortcut icon" href="img/title.png" /> 
 		<title>Dota MOC</title>
 	</head>
 	
-	<body>
-			
-		<div id="menuPrincipal">
+	<body>					
 			
 			<div id="menu">
 		
 					<ul>
-						<li id="primeiro"><a href="index.php"> Inicio </a></li>
+						<li id="primeiro"><a href="index.php" style="color: #FFF;font-size:19px;padding-left:23%;padding-right: 21.1%;"> Inicio </a></li>
 						<li><a href="#"> Campeonatos </a></li>
 						<li><a href="#"> Equipes MOC </a></li>
 						<li><a href="#"> Vencedores </a></li>
-						<li><a href="#"> Sugestões </a></li>
-						<li><a href="#"> Regras </a></li>
+						<li><a href="#"> Contato </a></li>
+						<li><a href="regras.html"> Regras </a></li>
 					</ul>
 			</div>
-				
-		</div>
+									
 		
 		<div id="corpo">
 				
@@ -37,7 +34,7 @@
 				
 				<div id="lateral">
 					
-				</div>
+				</div>							
 				
 		</div>
 	
